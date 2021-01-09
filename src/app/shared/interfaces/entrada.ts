02388031606d2bef9d3b09bf77fd68a60dae068b
ServerJon <ctsesiphon@hotmail.com>
2021-01-10 00:29:37 +0100

    crear estructura de datos sencilla

---
+++ src/app/shared/interfaces/entrada.ts
@@ -0,0 +1,4 @@
+export interface Entrada {
+  titulo: string;
+  resumen: string;
+}
--- src/app/views/listado/listado.component.ts
+++ src/app/views/listado/listado.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Entrada } from 'src/app/shared/interfaces/entrada';
+
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit {
+  // Atibutos
+  public listadoEntradas: Entrada[];
 
-  constructor() { }
+  constructor() {
+    this.listadoEntradas = [
+      {
+        titulo: 'Introducción a Angular',
+        resumen: 'En esta lección realizaremos una pequeña introducción al mundo de desarrollo con Angular'
+      },
+      {
+        titulo: 'Typescript como lenguaje para Angular',
+        resumen: 'Breve recorrido por el lenguaje de Typescript como base para desarrollar en Angular'
+      },
+      {
+        titulo: 'Componentes en Angular',
+        resumen: 'Aprenderemos a usar los componentes en Angular y el porqué de su importancia'
+      }
+    ];
+  }
 
   ngOnInit(): void {
   }
