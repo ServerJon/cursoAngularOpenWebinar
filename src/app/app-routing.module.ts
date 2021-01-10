60d7ae1d9025b1ec06970c3193d3a37961918a1c
ServerJon <ctsesiphon@hotmail.com>
2021-01-10 12:18:45 +0100

    añadiendo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import  { NgModule }  from  '@angular/core';
 import  { RouterModule, Routes }  from  '@angular/router';
 
-const  routes:  Routes  = [ ];
+import { ListadoComponent } from './views/listado/listado.component';
+import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
+import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
+
+const  routes:  Routes  = [
+  { path: 'listado', component: ListadoComponent },
+  { path: 'nosotros', component: AcercaDeNosotrosComponent},
+
+  { path: '', redirectTo: '/listado', pathMatch: 'full'},
+  { path: '**', component: PaginaNoEncontradaComponent}
+];
 
 @NgModule({
   exports: [RouterModule],
