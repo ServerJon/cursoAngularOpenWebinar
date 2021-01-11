63c8cfc1457fa9bfdfb2e171e4762cf3effe149e
ServerJon <ctsesiphon@hotmail.com>
2021-01-11 21:31:05 +0100

    Añadido formulario

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Usuario } from 'src/app/shared/classes/usuario';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // Atributos
+  public usuario: Usuario
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit(): void {
+  }
+
+  public submit(): void {
+    console.log('Usuario: ', this.usuario);
   }
 
 }
